5537f26ec80ce9c8e29e8d1484dbd5ef5c6e6088
thomas302 <mr.actually12@gmail.com>
2018-07-27 21:38:08 -0700

    Basic Structures

---
+++ csvFunctions.py
@@ -0,0 +1,12 @@
+#Functions pertaining to working with csv files
+class readFromCSV:
+    def main():
+
+class writeToCSV:
+    def addDir():
+
+    def addDriveIDToDir():
+
+    def main():
+
+
---
+++ gDriveFunctions.py
@@ -0,0 +1,10 @@
+#Functions used to control google drive.
+class upload:
+    def main():
+
+class download:
+    def main():
+
+if __name__=='__main__':
+    #Do test stuff
+
