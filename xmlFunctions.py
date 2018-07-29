25aa0b2290331735cf5807565e898c65616429d7
thomas302 <mr.actually12@gmail.com>
2018-07-28 22:32:00 -0700

    switched system of storing data from csv to xml

---
+++ xmlFunctions.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as et
+
+class readFromXML:
+    def __init__():
+       self.xmlTree = et.parse("dirList.xml")
+       self.root = self.xmlTree.getroot()
+    def readDirectory():
+
+    def readDriveID():
+
+class writeToXML:
+    def __init__():
+       self.xmlTree = et.parse("dirList.xml")
+       self.root =self.xmlTree.getroot()
+    def writeDirectory():
+
+    def writeDriveID():
+
+
