a71ad574137717aa58cf5aa565d3891f0fa0dfeb
thomas302 <mr.actually12@gmail.com>
2018-07-31 18:28:08 -0700

    XML Functions Now Working Properly

--- gDriveFunctions.py
+++ gDriveFunctions.py
@@ -1,5 +1,10 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 #Functions used to control google drive.
 class upload:
+    def __init__(self):
+        self.gAuth = GoogleAuth()
+        self.gAuth.
     def main():
 
 class download:
